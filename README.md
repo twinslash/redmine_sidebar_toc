bc6572b8075e57aa9d7174392be637aa68078511
Dmitriy Brodnitskiy <dmitriy.brodnitskiy@twinslash.com>
2013-03-14 14:50:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## redmine_sidebar_toc
+
+Provides navigation by the header of the anchor links in the wiki sidebar.
---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+Redmine::Plugin.register :redmine_sidebar_toc do
+  name 'Redmine Sidebar Toc plugin'
+  author '//Twinslash'
+  description 'navigation by the header of the anchor links in the wiki sidebar'
+  version '0.0.1'
+  url 'https://github.com/twinslash/redmine_sidebar_toc'
+  author_url 'twinslash.com'
+end
